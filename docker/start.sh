2c6179db990c56304be5a7b09050e1f1eba80687
goodallman <milan.pokorny27@gmail.com>
2026-07-22 20:08:28 +0200

    fix: normalize container entrypoint line endings

--- docker/start.sh
+++ docker/start.sh
@@ -3,10 +3,7 @@
 set -eu
 
 echo "Deploying prisma migrations"
-
 node ./node_modules/prisma/build/index.js migrate deploy --schema ./apps/web/prisma/schema.prisma
 
 echo "Starting web server"
-
 exec node apps/web/server.js
-
