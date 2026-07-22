398b9dc7a0783fd9697a49522168e1c8d99fd588
goodallman <milan.pokorny27@gmail.com>
2026-07-22 22:40:37 +0200

    fix: avoid dashboard auth loading regression

--- apps/web/src/app/(dashboard)/layout.tsx
+++ apps/web/src/app/(dashboard)/layout.tsx
@@ -1,16 +1,19 @@
 import { DashboardProvider } from "~/providers/dashboard-provider";
 import { NextAuthProvider } from "~/providers/next-auth";
+import { getServerAuthSession } from "~/server/auth";
 import { DashboardLayout } from "./dasboard-layout";
 
-export const dynamic = "force-static";
+export const dynamic = "force-dynamic";
 
-export default function AuthenticatedDashboardLayout({
+export default async function AuthenticatedDashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerAuthSession();
+
   return (
-    <NextAuthProvider>
+    <NextAuthProvider session={session}>
       <DashboardProvider>
         <DashboardLayout>{children}</DashboardLayout>
       </DashboardProvider>
